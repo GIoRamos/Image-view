41d9860e0984e62557db02b8ce0e791026dc20d6
GIOVANNI GARCIA <rl200005@rlas-macair-39.local>
2018-12-13 11:48:34 -0600

    Initial Commit

---
+++ Image view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image view
+//
+//  Created by GIOVANNI GARCIA on 9/6/18.
+//  Copyright © 2018 GIOVANNI GARCIA. All rights reserved.
+//
+
+import UIKit
+
+internal class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var myImage = #imageLiteral(resourceName: "liverpool-logo")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
